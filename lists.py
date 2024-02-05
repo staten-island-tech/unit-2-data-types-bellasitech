d74a452a78546d5627378a8018af85a9d77858a8
bellasitech <158211804+bellasitech@users.noreply.github.com>
2024-02-05 14:00:03 -0500

    e

---
+++ lists.py
@@ -0,0 +1,17 @@
+animals = ["zebra", "ape", "freshman"]
+#print(animals[0])
+
+""" for animals in animals: 
+    if(animal == "ape"):
+        print animal)
+""" 
+name = "jacky" 
+#print(name[0])
+# x = name.upper()
+# print(x)
+message = "hello league players, please use deoderant"
+words = message.split( )
+print(len(words)) 
+y = input("give me a number")
+print(y)
+print(y+9)
---
+++ listsgcf.py
@@ -0,0 +1,12 @@
+def gcf(x,y):
+      if(y == 0):
+        return x
+      else:
+       print(gcf)
+ 
+x = 20
+y = 15
+
+# prints 12
+print ("The gcf of 60 and 48 is : "12")
+print (gcf(20, 15))
