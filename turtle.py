05c9ca0136dcd61cbdbd44b90bb9087da8f609f7
bellasitech <158211804+bellasitech@users.noreply.github.com>
2024-02-06 13:49:46 -0500

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,16 @@
+factor = int(input ('Please type in the number you want me to factor'))
+factor1 = int(input ('Please type in the number you want me to factor'))
+
+
+def gcf(x):
+    for i in range(1, x+1): 
+        if((x % i == 0)): 
+            gcf = i
+            print(i)
+
+
+
+
+
+
+
