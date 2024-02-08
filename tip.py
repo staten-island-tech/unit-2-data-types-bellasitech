d1dabf3a78c4572b32c8ff7678c36bf60b2e94c5
bellasitech <158211804+bellasitech@users.noreply.github.com>
2024-02-08 14:00:30 -0500

    e

--- tip.py
+++ tip.py
@@ -1,11 +1,13 @@
-x = (int(input('total')))
-service = int(input ('please rate your service here'))
+bill = 100 
 
-if service == ("bad"):
-    print(float(x))
-elif service == ("okay"):
-    print(float(1.15*x))
-elif service == ("good"):
-    print(float(1.20*x))
-elif service == ("great"):
-    print(float(1.25*x))
+service = (input('please rate your service here')) 
+if service == "bad":
+    print(int(bill)*1)
+elif service == "okay":
+    print(int(bill)*1.15)
+elif service == "good":
+    print(int(bill)*1.20)
+elif service == "great":
+    print(int(bill)*1.25)
+else:
+    print("invalid response my guy") 
---
+++ tip2nd.py
@@ -0,0 +1,14 @@
+print("What was the value of the bill?")
+bill = input()
+print("How was the service?")
+service = input()
+if service == ("good"):
+    percentage = 0.20
+elif service == ("okay"):
+    percentage = 0.15
+elif service == ("bad"):
+    percentage = 0 
+elif service == ("great"):
+    percentage = 0.25
+tip = bill * percentage
+print(tip)
