62b1006c497aec20ffcdf78f0d3bfc033c63016d
bellasitech <158211804+bellasitech@users.noreply.github.com>
2024-02-15 14:09:27 -0500

    Create minecraft.py

---
+++ minecraft.py
@@ -0,0 +1,12 @@
+H = int(input('amount of health points bob has'))
+A = int(input('amount of apples bob eats'))
+S = int(input('amount of seconds bob waits'))
+
+def apple(): 
+    for i in range(1,h+1):
+        if((1<= H,A,S <= 10^5) and (1<= H,A,S <= 10^9)):
+            apple = i 
+        return(apple)
+    
+values = print(apple)
+     
